e5d07bd51904378c1753fb24b11b780ac6902393
Senad Djerlek <senajerlek@gmail.com>
2022-11-26 14:34:45 +0100

    added main site

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvaliableMeals from "./AvaliableMeals";
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary></MealsSummary>
+      <AvaliableMeals></AvaliableMeals>
+    </Fragment>
+  );
+};
+
+export default Meals;
